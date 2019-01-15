bec6022c033ce23259e9bb961bcf8222b7ce5c9f
daz261 <35951484+daz261@users.noreply.github.com>
2019-01-15 09:21:45 +0100

    Add files via upload

---
+++ NYUAD CampusTests/NYUAD_CampusTests.m
@@ -0,0 +1,39 @@
+//
+//  NYUAD_CampusTests.m
+//  NYUAD CampusTests
+//
+//  Created by Daria Zahaleanu on 3/11/18.
+//  Copyright © 2018 Daria Zahaleanu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NYUAD_CampusTests : XCTestCase
+
+@end
+
+@implementation NYUAD_CampusTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
